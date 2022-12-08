a7e8c055045623043983f137b44add1e99427b1c
AglendEka <117894471+AglendEka@users.noreply.github.com>
2022-12-08 16:36:13 +0700

    Add files via upload

---
+++ 1_C_71220884.py
@@ -0,0 +1,8 @@
+kata = input("masukan kata atau angka:")
+def reversed(kata):
+    str = ""
+    for i in kata:
+        str = i + str
+    return str
+print(reversed(kata))
+
---
+++ 2_C_71220884.py
@@ -0,0 +1,9 @@
+a = (input("Masukan angka:"))
+b = (input("Masukan angka yang dihitung:"))
+
+e = 0
+for c in list (a):
+    if c == b:
+        e = e+1
+print ("Angka",b,"Muncul sebanyak",e,"kali")
+        
